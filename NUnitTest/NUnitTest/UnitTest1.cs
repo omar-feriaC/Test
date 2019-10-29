56117b9a01b18ce9749647e1663794420d7d36f1
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-10-29 10:35:45 -0600

    commit

--- NUnitTest/NUnitTest/UnitTest1.cs
+++ NUnitTest/NUnitTest/UnitTest1.cs
@@ -1,13 +1,29 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
 
 namespace Tests
 {
     public class Tests
     {
+        private IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Url = "https://citrix2fa.sedgwickcms.com/logon/LogonPoint/index.html";
         }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
 
         [Test]
         public void Test1()
